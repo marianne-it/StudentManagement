8b8f5c5e2faf4ff347f86c66f53a0acf701d8e97
Marianne Elape <marianne.benilde.elape@gmail.com>
2025-02-14 20:35:00 +0800

    Initial commit

---
+++ src/com/raven/component/Header.java
@@ -0,0 +1,125 @@
+package com.raven.component;
+
+import java.awt.event.ActionListener;
+
+public class Header extends javax.swing.JPanel {
+
+    public Header() {
+        initComponents();
+    }
+
+    public void addMenuEvent(ActionListener event) {
+        cmdMenu.addActionListener(event);
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        cmdMenu = new com.raven.swing.Button();
+        pic = new com.raven.swing.ImageAvatar();
+        lbUserName = new javax.swing.JLabel();
+        lbRole = new javax.swing.JLabel();
+        jSeparator1 = new javax.swing.JSeparator();
+        buttonBadges1 = new com.raven.swing.ButtonBadges();
+        buttonBadges2 = new com.raven.swing.ButtonBadges();
+
+        setBackground(new java.awt.Color(50, 65, 140));
+
+        cmdMenu.setBackground(new java.awt.Color(50, 65, 140));
+        cmdMenu.setIcon(new javax.swing.ImageIcon(getClass().getResource("/com/raven/icon/menu.png"))); // NOI18N
+
+        pic.setIcon(new javax.swing.ImageIcon(getClass().getResource("/com/raven/icon/profile.jpg"))); // NOI18N
+
+        lbUserName.setFont(new java.awt.Font("sansserif", 1, 12)); // NOI18N
+        lbUserName.setForeground(new java.awt.Color(255, 255, 255));
+        lbUserName.setText("User Name");
+
+        lbRole.setForeground(new java.awt.Color(255, 255, 255));
+        lbRole.setText("Admin");
+
+        jSeparator1.setForeground(new java.awt.Color(255, 255, 255));
+        jSeparator1.setOrientation(javax.swing.SwingConstants.VERTICAL);
+
+        buttonBadges1.setBackground(new java.awt.Color(50, 65, 140));
+        buttonBadges1.setForeground(new java.awt.Color(250, 49, 49));
+        buttonBadges1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/com/raven/icon/notification.png"))); // NOI18N
+        buttonBadges1.setBadges(12);
+        buttonBadges1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                buttonBadges1ActionPerformed(evt);
+            }
+        });
+
+        buttonBadges2.setBackground(new java.awt.Color(50, 65, 140));
+        buttonBadges2.setForeground(new java.awt.Color(63, 178, 232));
+        buttonBadges2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/com/raven/icon/message.png"))); // NOI18N
+        buttonBadges2.setBadges(5);
+        buttonBadges2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                buttonBadges2ActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+        this.setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(cmdMenu, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 359, Short.MAX_VALUE)
+                .addComponent(buttonBadges2, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(buttonBadges1, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jSeparator1, javax.swing.GroupLayout.PREFERRED_SIZE, 8, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(lbUserName, javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(lbRole, javax.swing.GroupLayout.Alignment.TRAILING))
+                .addGap(18, 18, 18)
+                .addComponent(pic, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(lbUserName, javax.swing.GroupLayout.PREFERRED_SIZE, 16, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(lbRole, javax.swing.GroupLayout.PREFERRED_SIZE, 18, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addComponent(jSeparator1)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(cmdMenu, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(buttonBadges1, javax.swing.GroupLayout.PREFERRED_SIZE, 42, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(buttonBadges2, javax.swing.GroupLayout.PREFERRED_SIZE, 42, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(0, 0, Short.MAX_VALUE))))
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(pic, javax.swing.GroupLayout.PREFERRED_SIZE, 52, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 18, Short.MAX_VALUE))
+        );
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void buttonBadges1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_buttonBadges1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_buttonBadges1ActionPerformed
+
+    private void buttonBadges2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_buttonBadges2ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_buttonBadges2ActionPerformed
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private com.raven.swing.ButtonBadges buttonBadges1;
+    private com.raven.swing.ButtonBadges buttonBadges2;
+    private com.raven.swing.Button cmdMenu;
+    private javax.swing.JSeparator jSeparator1;
+    private javax.swing.JLabel lbRole;
+    private javax.swing.JLabel lbUserName;
+    private com.raven.swing.ImageAvatar pic;
+    // End of variables declaration//GEN-END:variables
+}
---
+++ src/com/raven/data/StudentsData.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.raven.data;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+
+/**
+ *
+ * @author hp
+ */
+public class StudentsData {
+    public static final Object[][] STUDENTS = {
+        {new ImageIcon(StudentsData.class.getResource("/com/raven/icon/profile.jpg")), "Marianne", "Male", "BSIT", 500},
+        {new ImageIcon(StudentsData.class.getResource("/com/raven/icon/profile1.jpg")), "Anne", "Female", "BSIT", 200},
+        {new ImageIcon(StudentsData.class.getResource("/com/raven/icon/profile2.jpg")), "Kate", "Male", "BSIT", 300},
+        {new ImageIcon(StudentsData.class.getResource("/com/raven/icon/profile2.jpg")), "Alex", "Male", "BSA", 0},
+        {new ImageIcon(StudentsData.class.getResource("/com/raven/icon/profile2.jpg")), "Alexa", "Female", "BSBA-FM", 0},
+        {new ImageIcon(StudentsData.class.getResource("/com/raven/icon/profile2.jpg")), "Moree", "Female", "BSBA-MM", 0},
+        {new ImageIcon(StudentsData.class.getResource("/com/raven/icon/profile2.jpg")), "Ana", "Female", "BSCE", 400},
+        {new ImageIcon(StudentsData.class.getResource("/com/raven/icon/profile2.jpg")), "Marie", "Female", "BSHM", 200},
+        {new ImageIcon(StudentsData.class.getResource("/com/raven/icon/profile2.jpg")), "Ann", "Female", "BSTM", 600},
+        {new ImageIcon(StudentsData.class.getResource("/com/raven/icon/profile2.jpg")), "Kate", "Female", "BSIT", 200},
+        {new ImageIcon(StudentsData.class.getResource("/com/raven/icon/profile2.jpg")), "Sky", "Male", "BSCPE", 300},
+        {new ImageIcon(StudentsData.class.getResource("/com/raven/icon/profile2.jpg")), "Luke", "Male", "BSA", 100},
+        {new ImageIcon(StudentsData.class.getResource("/com/raven/icon/profile2.jpg")), "Shaun", "Male", "BSIT", 0}
+    };
+}
