3a3f13d92c860eb21347684c3ab1684dd1b08bce
Marianne Elape <marianne.benilde.elape@gmail.com>
2025-02-17 16:03:00 +0800

    .

--- src/com/raven/data/StudentsData.java
+++ src/com/raven/data/StudentsData.java
@@ -1,29 +1,101 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.raven.data;
-import javax.swing.Icon;
-import javax.swing.ImageIcon;
 
-/**
- *
- * @author hp
- */
 public class StudentsData {
-    public static final Object[][] STUDENTS = {
-        {new ImageIcon(StudentsData.class.getResource("/com/raven/icon/profile.jpg")), "Marianne", "Female", "BSIT", 1},
-        {new ImageIcon(StudentsData.class.getResource("/com/raven/icon/profile1.jpg")), "Anne", "Female", "BSIT", 1},
-        {new ImageIcon(StudentsData.class.getResource("/com/raven/icon/profile2.jpg")), "Kate", "Female", "BSIT", 2},
-        {new ImageIcon(StudentsData.class.getResource("/com/raven/icon/profile2.jpg")), "Alex", "Male", "BSA", 3},
-        {new ImageIcon(StudentsData.class.getResource("/com/raven/icon/profile2.jpg")), "Alexa", "Female", "BSBA-FM", 1},
-        {new ImageIcon(StudentsData.class.getResource("/com/raven/icon/profile2.jpg")), "Moree", "Female", "BSBA-MM", 4},
-        {new ImageIcon(StudentsData.class.getResource("/com/raven/icon/profile2.jpg")), "Ana", "Female", "BSCE", 4},
-        {new ImageIcon(StudentsData.class.getResource("/com/raven/icon/profile2.jpg")), "Marie", "Female", "BSHM", 2},
-        {new ImageIcon(StudentsData.class.getResource("/com/raven/icon/profile2.jpg")), "Ann", "Female", "BSTM", 3},
-        {new ImageIcon(StudentsData.class.getResource("/com/raven/icon/profile2.jpg")), "Kate", "Female", "BSIT", 2},
-        {new ImageIcon(StudentsData.class.getResource("/com/raven/icon/profile2.jpg")), "Sky", "Male", "BSCPE", 3},
-        {new ImageIcon(StudentsData.class.getResource("/com/raven/icon/profile2.jpg")), "Luke", "Male", "BSA", 1},
-        {new ImageIcon(StudentsData.class.getResource("/com/raven/icon/profile2.jpg")), "Shaun", "Male", "BSIT", 4}
-    };
+
+    public static Iterable<Object[]> STUDENTS;
+    private String firstName;
+    private final String middleName;
+    private final String lastName;
+    private final String mobileNumber;
+    private final String email;
+    private final String dateOfBirth; 
+    private final String placeOfBirth;
+    private final String studentNumber;
+    private final String program;
+    private final String academicYear;
+    private final String entryLevel;
+
+    // Constructor
+    public StudentsData(String firstName, String middleName, String lastName, String mobileNumber, 
+                        String email, String dateOfBirth, String placeOfBirth, String studentNumber, 
+                        String program, String academicYear, String entryLevel) {
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.lastName = lastName;
+        this.mobileNumber = mobileNumber;
+        this.email = email;
+        this.dateOfBirth = dateOfBirth;
+        this.placeOfBirth = placeOfBirth;
+        this.studentNumber = studentNumber;
+        this.program = program;
+        this.academicYear = academicYear;
+        this.entryLevel = entryLevel;
+    }
+
+    // Getter and Setter Methods
+    public String getFirstName() { return firstName; }
+    public void setFirstName(String firstName) { this.firstName = firstName; }
+
+    // Repeat for all other fields...
+
+    @Override
+    public String toString() {
+        return "Student Data {" +
+                "First Name='" + firstName + '\'' +
+                ", Middle Name='" + middleName + '\'' +
+                ", Last Name='" + lastName + '\'' +
+                ", Mobile Number='" + mobileNumber + '\'' +
+                ", Email='" + email + '\'' +
+                ", Date of Birth='" + dateOfBirth + '\'' +
+                ", Place of Birth='" + placeOfBirth + '\'' +
+                ", Student Number='" + studentNumber + '\'' +
+                ", Program='" + program + '\'' +
+                ", Academic Year='" + academicYear + '\'' +
+                ", Entry Level='" + entryLevel + '\'' +
+                '}';
+    }
+
+    public String getemail() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public String getfirstName() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public String getmiddleName() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public String getlastName() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public String getmobileNumber() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public String getdateOfBirth() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public String getstudentNumber() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public String getplaceOfBirth() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public String getacademicYear() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public String getprogram() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public String getentryLevel() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
 }
